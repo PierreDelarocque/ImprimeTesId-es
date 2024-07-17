0dc0ccaf6231e8b46bddc53bdabbb8f64016fc2f
pierre delarocque <delarocque.pierre@gmail.com>
2024-07-17 10:11:40 +0200

    modification style and css

--- frontend/src/components/Videobg.jsx
+++ frontend/src/components/Videobg.jsx
@@ -1,12 +1,10 @@
-import video from "../assets/video/videobg.mp4";
-
 import "../styles/Accueil.css";
 
 function Videobg() {
   return (
     <div className="bg-container">
       <div className="overlay">
-        <video src={video} autoPlay loop muted></video>
+        {/* <video src={video} autoPlay loop muted></video>*/}
         <div className="titre-container">
           <h1 className="titre">Imprime Tes Idées</h1>
         </div>
